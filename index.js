180392d91d3c8c4324d6b689aef5ce9a30312c45
luqman-bashir <uthamin98@gmail.com>
2024-10-06 00:14:25 +0300

    eventslistener

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-function addingEventListener() {
-}
+function addingEventListener(){
+    const element= document.getElementById("button")
+    if(element){
+        element.addEventListener("click",function(){
+            console.log('Element clicked!');
+            
+        });
+    }else{
+            console.log('Element not found');
+            
+        }
+
+    }
+
+    module.exports = { addingEventListen};
+
+
+
+
+
+
